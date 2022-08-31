70b27847a3f955a1ae0f874b2e4fd26bfd9c1408
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-08-31 16:11:29 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,30 @@
 import { throttle } from 'lodash';
 
 const form = document.querySelector('form');
-const input = document.querySelector('input');
-const textArea = document.querySelector('textarea');
+const input = form.querySelector('input');
+const text = form.querySelector('textarea');
 
-form.addEventListener('input', throttle((event) => {
-    const obInput = { email: form.elements.email.value, message: form.elements.message.value };
-    localStorage.setItem('feedback-form-state', JSON.stringify(objectInput));
+form.addEventListener(
+'input',
+    throttle((event) => {
+        const input = { email: form.elements.email.value, message: form.elements.message.value };
+        localStorage.setItem('feedback-form-state', JSON.stringify(input));
 }, 500));
 
-form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const { elements: { email, message } } = event.currentTarget;
-      console.log({ email: email.value, message: message.value });
-      
-      e.currentTarget.reset();
-      localStorage.clear();
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { elements: { email, message } } = event.currentTarget;
+    console.log({ email: email.value, message: message.value });
+
+    event.currentTarget.reset();
+    localStorage.clear();
 });
+
 const storage = localStorage.getItem('feedback-form-state');
 const parseStorageData = JSON.parse(storage);
 const tryFoo = () => {
     if (parseStorageData !== null) {
-        email.value = parseStorageData.email;
+        input.value = parseStorageData.email;
         text.value = parseStorageData.message;
     };
 };
