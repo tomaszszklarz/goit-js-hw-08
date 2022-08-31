360c7df0c94ef54aa4fc5113a586b0bc93b02ca3
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-08-31 13:44:05 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,28 +2,27 @@ import Player from '@vimeo/player';
 import { throttle } from 'lodash';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('timeupdate',throttle(({ seconds }) => {
-    localStorage.setItem('videoplayer-current-time', `${seconds}`);
-  }, 1000)
-);
+player.on("timeupdate", throttle(({ duration, percent, seconds }) => {
+    localStorage.setItem("videoplayer-current-time", `${seconds}`);
+    
+}, 1000));
 
-const time = localStorage.getItem('videoplayer-current-time');
+const time = localStorage.getItem("videoplayer-current-time");
 
 player
   .setCurrentTime(time)
   .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
+        // seconds = the actual time that the player seeked to
+    }).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
 
-      default:
-        // some other error occurred
-        break;
-    }
-  });
+            default:
+                // some other error occurred
+                break;
+        }
+    });
